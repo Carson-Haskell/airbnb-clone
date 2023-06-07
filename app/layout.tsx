46df938da159b779c070fd002eb404afb0780beb
Carson Haskell <carson.haskell@gmail.com>
2023-06-06 17:35:20 -0700

    fix hydration issue with ClientOnly.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import Navbar from './components/navbar/Navbar';
-import './globals.css'
-import { Nunito } from 'next/font/google';
+import ClientOnly from "./components/ClientOnly";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
 const font = Nunito({
   subsets: ["latin"],
-})
+});
 
 export const metadata = {
-  title: 'Airbnb | Clone',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb | Clone",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
